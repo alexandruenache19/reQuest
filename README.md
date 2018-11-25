75e92ecfedb87995af82176bf7707159fb3cca62
Radu Mazilu <maziluradugabriel@gmail.com>
2018-11-25 19:28:25 +0000

    change package.json

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-## Simple ToDo application using React Redux and Firebase
-
-![Preview](http://g.recordit.co/tMEezVSBwL.gif)
-
-## Project Overview
-
-This is a simple application for managing To Do tasks. Purpose of this application was to demonstrate how to combine Firebase with React and Redux
+## A place to share and answer questions in your team
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,7 @@
 import * as firebase from "firebase";
 
 var config = {
-  apiKey: "AIzaSyDFKCtD3y9rQ6S0jxUge7LqHG9huwxkaFc",
-  authDomain: "meta-pay-review.firebaseapp.com",
-  databaseURL: "https://meta-pay-review.firebaseio.com",
-  projectId: "meta-pay-review",
-  storageBucket: "meta-pay-review.appspot.com",
-  messagingSenderId: "1010968158287"
+  
 };
 
 firebase.initializeApp(config);
